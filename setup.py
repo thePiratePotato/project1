ad8cc7c516733206c25e7bb18390b11d215a6778
vuhoangninh1998 <38416846+vuhoangninh1998@users.noreply.github.com>
2019-09-18 13:42:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from flaskblog import create_app
 
 app =  create_app()
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=80,debug=True)
